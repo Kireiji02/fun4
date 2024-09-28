ed9571fb942c930f3480d59fcdba25ef5362085c
Kireiji02 <martmanaswin@gmail.com>
2024-09-28 10:42:15 +0700

    Update keyboard_teleop.py

--- scripts/keyboard_teleop.py
+++ scripts/keyboard_teleop.py
@@ -15,6 +15,7 @@ KEY_BINDINGS = {
     '3': 3
     }
 
+
 def get_key(settings):
     """
     Capture key input from the terminal.
@@ -33,6 +34,9 @@ class TeleopNode(Node):
         #----------------------------Variables----------------------------#
         
         self.latest = 0
+        self.mode1_value = ['','','']
+        self.trig = 'neutral'
+        self.value_array = [0.0,0.0,0.0]
 
         #----------------------------Timer----------------------------#
         
@@ -74,8 +78,7 @@ class TeleopNode(Node):
             
             
     ---------------- Mode Selection ---------------- 
-
-                          
+     
                           
                        1   2   3
 
@@ -83,11 +86,9 @@ class TeleopNode(Node):
     2: Teleoperation
     3: Automatic
     
-    c: Mode Confirm
-    
             """
         )
-        if key in KEY_BINDINGS:
+        if key in KEY_BINDINGS and self.trig == 'neutral':
             self.send_flag(KEY_BINDINGS[key])
             self.latest = KEY_BINDINGS[key]
             print(f'          Selecting Mode : [ {KEY_BINDINGS[key]} ]')
@@ -95,17 +96,145 @@ class TeleopNode(Node):
             self.mode_init()
             print(f'          Mode Change Applied : [ {self.latest} ]')
         elif key == "\x03" :
-            rclpy.shutdown()
+            rclpy.shutdown()        
+        elif key == "\x09" and self.latest == 1 and self.trig == 'neutral':
+            print(f'     mode 1 input : x = []')
+            self.trig = 'trig_x'
+            
+        elif self.latest == 1 and self.trig == 'trig_x' and (key.isnumeric() or key == '.' or key == '-' or key == "\x09"):
+            if key == "\x09":
+                self.trig = 'trig_y'
+                print(f'     mode 1 input : x = [{self.mode1_value[0]}]\n                    y = [] (tab for next)')
+                if len(self.mode1_value[0])==0:
+                    self.value_array[0] = 0.0
+            if key == '-':
+                if len(self.mode1_value[0]) == 0:
+                    self.mode1_value[0] += '-'
+                    print(f'     mode 1 input : x = [{self.mode1_value[0]}] (tab for next)')
+                else:
+                    print(f'     mode 1 input : x = [{self.mode1_value[0]}] (tab for next)')
+                    print(f'\n        >> Please input number only <<')
+            if key.isnumeric():
+                self.mode1_value[0] += key
+                print(f'     mode 1 input : x = [{self.mode1_value[0]}] (tab for next)')
+            if key == '.':
+                self.trig = 'trig_x.'
+                if len(self.mode1_value[0]) == 0:
+                    self.mode1_value[0] += '0.'
+                else:
+                    self.mode1_value[0] += key
+                print(f'     mode 1 input : x = [{self.mode1_value[0]}] (tab for next)')
+        elif self.latest == 1 and self.trig == 'trig_x' and key.isnumeric() == False:   
+            print(f'     mode 1 input : x = [{self.mode1_value[0]}]')
+            print(f'\n        >> Please input number only <<')
+        elif self.latest == 1 and self.trig == 'trig_x.' and (key.isnumeric() or key == "\x09"):
+            if key == "\x09":
+                print(f'     mode 1 input : x = [{self.mode1_value[0]}]\n                    y = [] (tab for next)')
+                self.trig = 'trig_y'
+                if len(self.mode1_value[0])==0:
+                    self.value_array[0] = 0.0
+            if key.isnumeric():
+                self.mode1_value[0] += key
+                print(f'     mode 1 input : x = [{self.mode1_value[0]}] (tab for next)')
+        elif self.latest == 1 and self.trig == 'trig_x.' and key.isnumeric() == False:   
+            print(f'     mode 1 input : x = [{self.mode1_value[0]}] (tab for next)')
+            print(f'\n        >> Please input number only <<')
+            
+        elif self.latest == 1 and self.trig == 'trig_y' and (key.isnumeric() or key == '.' or key == '-' or key == "\x09"):
+            if key == "\x09":
+                self.trig = 'trig_z'
+                print(f'     mode 1 input : x = [{self.mode1_value[0]}]\n                    y = [{self.mode1_value[1]}]\n                    z = [] (tab for next)')
+                if len(self.mode1_value[1])==0:
+                    self.value_array[1] = 0.0
+            if key == '-':
+                if len(self.mode1_value[1]) == 0:
+                    self.mode1_value[1] += '-'
+                    print(f'     mode 1 input : x = [{self.mode1_value[0]}]\n                    y = [{self.mode1_value[1]}] (tab for next)')
+                else:
+                    print(f'     mode 1 input : x = [{self.mode1_value[0]}]\n                    y = [{self.mode1_value[1]}] (tab for next)')
+                    print(f'\n        >> Please input number only <<')
+            if key.isnumeric():
+                self.mode1_value[1] += key
+                print(f'     mode 1 input : x = [{self.mode1_value[0]}]\n                    y = [{self.mode1_value[1]}] (tab for next)')
+            if key == '.':
+                self.trig = 'trig_y.'
+                if len(self.mode1_value[1]) == 0:
+                    self.mode1_value[1] += '0.'
+                else:
+                    self.mode1_value[1] += key
+                print(f'     mode 1 input : x = [{self.mode1_value[0]}]\n                    y = [{self.mode1_value[1]}] (tab for next)')
+        elif self.latest == 1 and self.trig == 'trig_y' and key.isnumeric() == False:   
+            print(f'     mode 1 input : x = [{self.mode1_value[0]}]\n                    y = [{self.mode1_value[1]}] (tab for next)')
+            print(f'\n        >> Please input number only <<')
+        elif self.latest == 1 and self.trig == 'trig_y.' and (key.isnumeric() or key == "\x09"):
+            if key == "\x09":
+                print(f'     mode 1 input : x = [{self.mode1_value[0]}]\n                    y = [{self.mode1_value[1]}]\n                    z = [] (tab for next)')
+                self.trig = 'trig_z'
+                if len(self.mode1_value[1])==0:
+                    self.value_array[1] = 0.0
+            if key.isnumeric():
+                self.mode1_value[1] += key
+                print(f'     mode 1 input : x = [{self.mode1_value[0]}]\n                    y = [{self.mode1_value[1]}] (tab for next)')
+        elif self.latest == 1 and self.trig == 'trig_y.' and key.isnumeric() == False:   
+            print(f'     mode 1 input : x = [{self.mode1_value[0]}]\n                    y = [{self.mode1_value[1]}] (tab for next)')
+            print(f'\n        >> Please input number only <<')
+            
+        elif self.latest == 1 and self.trig == 'trig_z' and (key.isnumeric() or key == '.' or key == '-' or key == "\x09"):
+            if key == "\x09":
+                self.trig = 'trig_end'
+                print(f'     mode 1 input : x = [{self.mode1_value[0]}]\n                    y = [{self.mode1_value[1]}]\n                    z = [{self.mode1_value[2]}] (tab for next)')
+                if len(self.mode1_value[2])==0:
+                    self.value_array[2] = 0.0
+            if key == '-':
+                if len(self.mode1_value[2]) == 0:
+                    self.mode1_value[2] += '-'
+                    print(f'     mode 1 input : x = [{self.mode1_value[0]}]\n                    y = [{self.mode1_value[1]}]\n                    z = [{self.mode1_value[2]}] (tab for next)')
+                else:
+                    print(f'     mode 1 input : x = [{self.mode1_value[0]}]\n                    y = [{self.mode1_value[1]}]\n                    z = [{self.mode1_value[2]}] (tab for next)')
+                    print(f'\n        >> Please input number only <<')
+            if key.isnumeric():
+                self.mode1_value[2] += key
+                print(f'     mode 1 input : x = [{self.mode1_value[0]}]\n                    y = [{self.mode1_value[1]}]\n                    z = [{self.mode1_value[2]}] (tab for next)')
+            if key == '.':
+                self.trig = 'trig_z.'
+                if len(self.mode1_value[2]) == 0:
+                    self.mode1_value[2] += '0.'
+                else:
+                    self.mode1_value[2] += key
+                print(f'     mode 1 input : x = [{self.mode1_value[0]}]\n                    y = [{self.mode1_value[1]}]\n                    z = [{self.mode1_value[2]}] (tab for next)')
+        elif self.latest == 1 and self.trig == 'trig_z' and key.isnumeric() == False:   
+            print(f'     mode 1 input : x = [{self.mode1_value[0]}]\n                    y = [{self.mode1_value[1]}]\n                    z = [{self.mode1_value[2]}] (tab for next)')
+            print(f'\n        >> Please input number only <<')
+        elif self.latest == 1 and self.trig == 'trig_z.' and (key.isnumeric() or key == "\x09"):
+            if key == "\x09":
+                print(f'     mode 1 input : x = [{self.mode1_value[0]}]\n                    y = [{self.mode1_value[1]}]\n                    z = [{self.mode1_value[2]}] (tab for next)')
+                self.trig = 'trig_end'
+                if len(self.mode1_value[2])==0:
+                    self.value_array[2] = 0.0
+            if key.isnumeric():
+                self.mode1_value[2] += key
+                print(f'     mode 1 input : x = [{self.mode1_value[0]}]\n                    y = [{self.mode1_value[1]}]\n                    z = [{self.mode1_value[2]}] (tab for next)')
+        elif self.latest == 1 and self.trig == 'trig_z.' and key.isnumeric() == False:   
+            print(f'     mode 1 input : x = [{self.mode1_value[0]}]\n                    y = [{self.mode1_value[1]}]\n                    z = [{self.mode1_value[2]}] (tab for next)')
+            print(f'\n        >> Please input number only <<')
+        elif self.latest == 1 and self.trig == 'trig_end':
+            self.value_array[0] = float(self.mode1_value[0])
+            self.value_array[1] = float(self.mode1_value[1])
+            self.value_array[2] = float(self.mode1_value[2])
+            
+            print(f'     value ready : {self.value_array}, press c')
+        
+        if key == '1' and self.trig == 'neutral':
+            print('\n        >> Press tab to input target value <<')
+        
         print(
             """
-
-        >> Exit upon pressing any other keys <<
         
-
-    ------------------------------------------------
-
-        """
+    c: Mode Confirmation
+    ------------------------------------------------"""
         )
+        print(f'{key}',self.trig,self.latest,self.mode1_value,self.value_array)
+        
         
 
 def main(args=None):
